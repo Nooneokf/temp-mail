941e446221c27b06bffeb5f2d90694753d156454
Dishant Singh <122022552+DishantSinghDev2@users.noreply.github.com>
2024-11-30 18:12:43 +0000

    Update layout metadata and integrate Google Analytics for improved tracking

--- temp/app/layout.tsx
+++ temp/app/layout.tsx
@@ -5,12 +5,12 @@ export const metadata = {
   // Open Graph
   ogTitle: 'Free Custom Temporary Email Address - Temp Mail',
   ogDescription: 'Keep your real email inbox clean. Temporary emails are perfect for any transaction where you want to improve your online privacy. Use our disposable email addresses for ad blocking, no spam, no ads, just temporary mail.',
-  ogImage: 'https://tempmail.dev/og-image.jpg',
-  ogUrl: 'https://tempmail.dev/',
+  ogImage: 'https://freecustom.email/logo.png',
+  ogUrl: 'https://freecustom.email/',
   // Target Keywords
   keywords: 'temp mail, custom mail, disposable email, temporary email, fake email, email privacy, spam-free email, temporary inbox'
 }
-
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function RootLayout({
   children,
@@ -20,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+      <GoogleAnalytics gaId="G-RXTEEVC8C4" />
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body>{children}</body>
