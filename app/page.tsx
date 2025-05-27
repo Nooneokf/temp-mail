8ef1102a7595ed04bdedec1cd79f57744267658d
Dishant Singh <dishupandey57@gmail.com>
2025-05-27 12:50:56 +0000

    feat: enhance heading styling for improved responsiveness and readability

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,14 @@ export default function Page() {
         <main className="mx-auto m-2 px-4 py-8">
           <section className="mb-12">
             <EmailBox />
-            <h1 className="mt-6 text-3xl font-bold">The Tech behind Disposable Temp Mail Address</h1>
+            <h1
+              className="mt-6 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold"
+              suppressHydrationWarning
+            >
+              The Tech behind Disposable Temp Mail Address
+            </h1>
             <p className="mb-4 text-muted-foreground">
-            We all use email for various purposes, from connecting at work and with business prospects to communicating with friends. Yet, with the increase in online services requiring email addresses, spam and privacy concerns have grown. Freecustom.email helps protect your real email identity and keeps you safe from unwanted communications by offering a temporary, disposable email address.
+              We all use email for various purposes, from connecting at work and with business prospects to communicating with friends. Yet, with the increase in online services requiring email addresses, spam and privacy concerns have grown. Freecustom.email helps protect your real email identity and keeps you safe from unwanted communications by offering a temporary, disposable email address.
             </p>
           </section>
           <WhySection />
